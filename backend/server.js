6ba27400ee37b2937a339ed07827fc98ceee0090
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-03-31 20:07:16 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import authRoutes from "./routes/auth.routes.js"
-import connectToMongoDB from "./db/connectToMongoDB.js";
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000
@@ -16,6 +18,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, ()=>{
     connectToMongoDB()
